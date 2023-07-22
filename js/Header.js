5cedeb782a1dcb930eac7307f5613f3758edf561
happy1511 <140176910+happy1511@users.noreply.github.com>
2023-07-22 17:53:38 +0000

    day1

---
+++ js/Header.js
@@ -0,0 +1,14 @@
+var open = 0
+
+function toggleMenu(){
+    const tag = document.getElementById('menu');
+
+    if(open == 0){
+        tag.style.display = "inline-block"
+        open = 1
+    }
+    else{
+        tag.style.display = "none"
+        open = 0
+    }
+}
